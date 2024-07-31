7c338e671d2c0f4c2aa32bc8d53d75c6c7bddce3
Wifi1230 <oliwier.zar@wp.pl>
2024-07-31 19:04:32 +0200

    koniec dzien 3

--- src/bootcamp_chat_frontend/src/App.vue
+++ src/bootcamp_chat_frontend/src/App.vue
@@ -5,6 +5,7 @@ import { AuthClient } from '@dfinity/auth-client';
 import { HttpAgent } from '@dfinity/agent';
 import type { Identity } from '@dfinity/agent';
 import { Principal } from '@dfinity/principal';
+import type { UserData } from '../../declarations/bootcamp_chat_backend/bootcamp_chat_backend.did';
 
 export default {
   data() {
@@ -12,49 +13,97 @@ export default {
       newChat: "",
       chats: [] as string[][],
       identity: undefined as undefined | Identity,
-      principalText: "",
+      principal: undefined as undefined | Principal,
       targetPrincipal: "",
+      userData: undefined as undefined | UserData,
+      newUsername: "",
+      allUsers: [] as [Principal, UserData][]
     }
   },
   methods: {
-    async dodajChatMSG() {
-      if (!this.identity || this.identity.getPrincipal() === Principal.anonymous()) {
+    isUserLogged() {
+      if (!this.identity || !this.principal || this.principal === Principal.anonymous()) {
         throw new Error("PLZ log in")
       }
-      const targetPrincipal = Principal.fromText(this.targetPrincipal)
+      return {
+        identity: this.identity,
+        principal: this.principal 
+      }
+    },
+    validateTargetPrincipal(){
+      const cleanTargetPrincipal = this.targetPrincipal.trim();
+      if (cleanTargetPrincipal === ""){
+        throw new Error("No principal")
+      }
+      const targetPrincipal = Principal.fromText(cleanTargetPrincipal)
       if (!targetPrincipal || targetPrincipal === Principal.anonymous()){
         throw new Error("Wrong target")
       }
-
-      const backend = createActor(canisterId, {
+      return targetPrincipal
+    },
+    getAuthClient(){
+      this.isUserLogged()
+      return createActor(canisterId, {
         agentOptions: {
           identity: this.identity
         }
       });
+    },
+    async dodajChatMSG() {
+      const targetPrincipal = this.validateTargetPrincipal()
+      const backend = this.getAuthClient();
       await backend.add_chat_msg(this.newChat, targetPrincipal)
       await this.pobierzChaty()
     },
     async pobierzChaty() {
-      if (!this.identity || this.identity.getPrincipal() === Principal.anonymous()) {
-        throw new Error("PLZ log in")
-      }
-      const targetPrincipal = Principal.fromText(this.targetPrincipal)
-      if (!targetPrincipal || targetPrincipal === Principal.anonymous()){
-        throw new Error("Wrong target")
-      }
-      this.chats = await bootcamp_chat_backend.get_chat(this.identity.getPrincipal(), targetPrincipal)
+      const {identity, principal} = this.isUserLogged()
+      const targetPrincipal = this.validateTargetPrincipal()
+
+      const chatPath = [targetPrincipal, identity.getPrincipal()].sort()
+      this.chats = await bootcamp_chat_backend.get_chat(chatPath)
     },
     async login() {
       const authClient = await AuthClient.create();
       await authClient.login({
-        identityProvider: "http://avqkn-guaaa-aaaaa-qaaea-cai.localhost:4943/"
+        identityProvider: "http://avqkn-guaaa-aaaaa-qaaea-cai.localhost:4943/",
+        onSuccess: async () => {
+          const identity = authClient.getIdentity();
+          const principal = identity.getPrincipal();
+          this.principal = principal;
+          this.identity = identity;
+          console.log("Zalogowano", this.principal)
+          await this.getUserData()
+          await this.getAllUsers()
+        }
       })
-
-      const identity = authClient.getIdentity();
-      this.principalText = identity.getPrincipal().toText()
-      console.log("Zalogowano", this.principalText)
-      this.identity = identity;
-      await this.pobierzChaty()
+    },
+    async logout () {
+      const authClient = await AuthClient.create();
+      await authClient.logout()
+      this.identity = undefined;
+      this.principal = undefined;
+      this.chats = [];
+      this.userData = undefined
+    },
+    async registerUsername() {
+      const trimedUsername = this.newUsername.trim();
+      const backend = this.getAuthClient();
+      await backend.register(trimedUsername)
+      await this.getUserData()
+      await this.getAllUsers()
+    },
+    async getUserData() {
+      const {principal} = this.isUserLogged()
+      const maybeUserData = await bootcamp_chat_backend.get_user(principal as Principal)
+      if (maybeUserData.length === 0) {
+        this.userData = undefined
+      } else {
+        this.userData = maybeUserData[0]
+      }
+      console.log("User data", this.userData)
+    },
+    async getAllUsers(){
+      this.allUsers = await bootcamp_chat_backend.get_users()
     }
   },
 }
@@ -62,20 +111,27 @@ export default {
 
 <template>
   <main>
-    <img src="/logo2.svg" alt="DFINITY logo" />
-    <br />
-    <br />
-    {{ principalText }} <button @click="login">login</button>
-    <div>
-      <input v-model="targetPrincipal" /><button @click="pobierzChaty">pobierz chat</button>
+    <button v-if="!principal" @click="login">login</button>
+    <button v-if="principal" @click="logout">logout</button>
+    <div v-if="principal && !userData">
+      <input v-model="newUsername" placeholder="nick"/> <button @click="registerUsername">register</button>
     </div>
-    <div>
-      <div v-for="chat in chats[0]">
-        {{ chat }}
+    <div v-if="principal && userData">
+      {{ userData.nickname }}
+      <div v-if="allUsers">
+        <select v-model="targetPrincipal">
+          <option disabled value="">Please select one</option>
+          <option v-for="[userPrincipal, userData] in allUsers" :value="userPrincipal.toText()">{{ userData.nickname }}</option>
+        </select>
       </div>
-    </div>
-    <div>
-      <textarea v-model="newChat"></textarea><button @click="dodajChatMSG">Dodaj notatke</button>
+      <div>
+        <div v-for="chat in chats[0]">
+          {{ chat }}
+        </div>
+      </div>
+      <div>
+        <textarea v-model="newChat" placeholder="wiadomosc"></textarea><button @click="dodajChatMSG">Dodaj notatke</button>
+      </div>
     </div>
   </main>
 </template>
